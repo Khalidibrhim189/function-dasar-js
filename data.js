c4795fd0a8b9ee36f2eb622a6615e67c4cb757d6
Khalid Ibrahim <khalid.jambi18@gmail.com>
2021-03-21 16:38:00 +0700

    function-dasar

---
+++ data.js
@@ -0,0 +1,8 @@
+function biodata(nama, umur, asal){
+    document.write('<br> Nama lengkap :' + nama);
+    document.write('<br> Umur :' + umur);
+    document.write('<br> Asal :' + asal);
+}
+
+biodata('Khalid Ibrahim', 22, 'Kota jambi');
+
